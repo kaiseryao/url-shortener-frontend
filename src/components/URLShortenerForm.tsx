ed985a5c02b008cbeccec0e79327c110a3206676
Yao Kaiser <yao@ywk.ch>
2021-10-22 16:06:06 +0200

    update: style, new color codes

--- src/components/URLShortenerForm.tsx
+++ src/components/URLShortenerForm.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 function URLShortenerForm(){
     return <div>URL Shortenre</div>
 }
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,46 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      colors: {
+      primary: "#242038",
+      hover: "#332D4F",
+      secondary: "#C3BDE1",
+      success: "#22EF8B",
+      danger: "#DC2626",
+      bgDark: "#092012",
+      bgLight: "#FFFFFF",
+    },
+    }
   },
   variants: {
     extend: {},
