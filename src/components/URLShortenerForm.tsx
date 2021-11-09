c92c95498133570143e227f0cdcc2a37fcecadd7
Yao Kaiser <yao@ywk.ch>
2021-11-09 14:49:48 +0100

    design: icon and text on same line

--- src/components/URLShortenerForm.tsx
+++ src/components/URLShortenerForm.tsx
@@ -17,18 +17,13 @@ function URLShortenerForm() {
             className="w-full py-2 pl-3 pr-10 transition-colors border-2 border-gray-200 rounded-xl hover:border-gray-300 focus:outline-none focus:border-blue-500"
             placeholder="Enter your URL"
           />
-          <button className="absolute block text-sm text-center text-gray-400 transition-colors w-26 h-7 leading-0 top-2 right-5 focus:outline-none hover:text-gray-900">
+          <button className="absolute inline-flex items-center text-sm text-center text-gray-400 transition-colors w-26 h-7 leading-0 top-2 right-5 focus:outline-none hover:text-gray-900">
             Shorten <Icon icon="akar-icons:arrow-right" />
           </button>
         </div>
  
         <div className="absolute top-0 left-0 flex w-full h-2">
           <div className="flex-1 h-2 bg-red-500"></div>
-          <div className="flex-1 h-2 bg-yellow-500"></div>
-          <div className="flex-1 h-2 bg-yellow-300"></div>
-          <div className="flex-1 h-2 bg-green-500"></div>
-          <div className="flex-1 h-2 bg-blue-500"></div>
-          <div className="flex-1 h-2 bg-purple-700"></div>
         </div>
       </div>
     </div>
@@ -38,3 +33,11 @@ function URLShortenerForm() {
 export default URLShortenerForm;
 
 //<script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>;
+/* Rainbow Colors
+          <div className="flex-1 h-2 bg-red-500"></div>
+          <div className="flex-1 h-2 bg-yellow-500"></div>
+          <div className="flex-1 h-2 bg-yellow-300"></div>
+          <div className="flex-1 h-2 bg-green-500"></div>
+          <div className="flex-1 h-2 bg-blue-500"></div>
+          <div className="flex-1 h-2 bg-purple-700"></div>
+*/
